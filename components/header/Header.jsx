5854f2ae4a7dea7e976d4af33c2b86bad234b693
ashpougengit <cyprusandaxi@gmail.com>
2024-09-24 16:50:53 +0545

    Knowledgebase Menu added on the Navbar

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -6,34 +6,71 @@ import Image from 'next/image';
 function Header() {
 
     return (
-        <header>
-            <div className="header">
-                <div className="navigation-bar">
-                    <div className="logo">
-                        <Link href='/'>
-                            <Image src="/images/comparedoo-logo.png" fill alt="Comparedoo Official Logo" />
-                        </Link>
-                    </div>
+      <header>
+        <div className="header">
+          <div className="navigation-bar">
+            <div className="logo">
+              <Link href="/">
+                <Image
+                  src="/images/comparedoo-logo.png"
+                  fill
+                  alt="Comparedoo Official Logo"
+                />
+              </Link>
+            </div>
 
-                    <div className="nav-menus">
-                        <ul>
-                            <li><Link className='next-link-header' href='/north-america'>North America</Link></li>
-                            <li><Link className='next-link-header' href='/south-america'>South America</Link></li>
-                            <li><Link className='next-link-header' href='/oceania'>Oceania</Link></li>
-                            <li><Link className='next-link-header' href='/europe'>Europe</Link></li>
-                            <li><Link className='next-link-header' href='/asia'>Asia</Link></li>
-                            <li><Link className='next-link-header' href='/africa'>Africa</Link></li>
-                            <li><Link className='next-link-header' href='/antartica'>Antarctica</Link></li>
-                        </ul>
-                    </div>
+            <div className="nav-menus">
+              <ul>
+                <li>
+                  <Link className="next-link-header" href="/north-america">
+                    N.America
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/south-america">
+                    S.America
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/oceania">
+                    Oceania
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/europe">
+                    Europe
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/asia">
+                    Asia
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/africa">
+                    Africa
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/antartica">
+                    Antarctica
+                  </Link>
+                </li>
+                <li>
+                  <Link className="next-link-header" href="/antartica">
+                    Knowledgebase
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-                    {/* <Link href='/menu-bar' className="hamburger-icon" >
+            {/* <Link href='/menu-bar' className="hamburger-icon" >
                         ☰
                     </Link> */}
-                    <HamburgerIcon />
-                </div>
-            </div>
-        </header>
+            <HamburgerIcon />
+          </div>
+        </div>
+      </header>
     );
 }
 
