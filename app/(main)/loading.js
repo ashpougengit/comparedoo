a79357c5e3fb5f60eaf8ae5974d749029efa1d8c
UNSTOPABLE SAGAR <unstopable.sagar@gmail.com>
2024-10-09 17:37:40 +0545

    deleted unnecessary files

--- app/(main)/loading.js
+++ app/(main)/loading.js
@@ -1,13 +1,3 @@
-// import classes from './loading.module.css'
-
-// function LoadingPage() {
-//     return (
-//         <p className={classes.loading}>Loading data...</p>
-//     )
-// }
-
-// export default LoadingPage
-
 import classes from './loading.module.css';
 
 function LoadingPage() {
