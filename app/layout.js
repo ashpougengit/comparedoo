3f53d543ea7a0c895ee419b878d94332dbc6bf63
UNSTOPABLE SAGAR <unstopable.sagar@gmail.com>
2024-10-15 15:43:11 +0545

    Updated Google Tag Manager code

--- app/layout.js
+++ app/layout.js
@@ -47,22 +47,16 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="bpl0jhFx1HsUh0FC9Aqd6HsB6NkAfzDx51KP8Ao6hug" />
 
         {/* Google Tag Manager Start */}
-        <Script id="google-tag-manager" strategy="afterInteractive">{/* "beforeInteractive" or "lazyOnload" */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FJTLNMJTNT"
+        />
+        <Script id="google-analytics">
           {`
-          (function(w, d, s, l, i) {
-            w[l] = w[l] || []; 
-            w[l].push({
-              'gtm.start': new Date().getTime(), 
-              event: 'gtm.js'
-            }); 
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s), 
-              dl = l != 'dataLayer' ? '&l=' + l : ''; 
-            j.async = true; 
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; 
-            f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-5VSW5DGK');
-        `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FJTLNMJTNT');
+          `}
         </Script>
         {/* Google Tag Manager End */}
 
